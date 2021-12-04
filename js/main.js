920c9e0566707a48047e6cb27587960301573170
torontotoki <95222411+torontotoki@users.noreply.github.com>
2021-12-04 10:42:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+ScrollReveal().reveal('.top__title', {
+    origin: 'left',
+    delay: 600,
+    distance: '15px',
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.top__subtitle', {
+    origin: 'left',
+    delay: 900,
+    distance: '15px',
+    easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.header__inner', {
+    origin: 'top',
+    delay: 400,
+    distance: '15px',
+    easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.header', {
+    origin: 'top',
+    delay: 200,
+    distance: '15px',
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.top__img-arrow', {
+    origin: 'left',
+    delay: 1000,
+    distance: '15px',
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.top__btn', {
+    origin: 'bottom',
+    delay: 1200,
+    distance: '15px',
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.top__image', {
+    origin: 'right',
+    delay: 400,
+    distance: '15px',
+    easing: 'ease-in-out',
+});
